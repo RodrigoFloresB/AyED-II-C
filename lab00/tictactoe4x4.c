fdc0e0b42a10ea9929142ec5e64a492d8e2b550d
RoodriiFb <roodrii.flores.rf@gmail.com>
2023-03-20 22:12:40 -0300

    Tateti

---
+++ lab00/tictactoe4x4.c
@@ -0,0 +1,107 @@
+#include <stdlib.h>  /* exit() y EXIT_FAILURE */
+#include <stdio.h>   /* printf(), scanf()     */
+#include <stdbool.h> /* Tipo bool             */
+
+#define VERDE_T "\x1b[32m"
+#define RESET_COLOR "\x1b[0m" /* Color para Texto */
+
+#include <assert.h>  /* assert() */
+
+#define CELL_MAX (3 * 3 - 1)
+
+void print_sep(int length) {
+    printf("\t ");
+    for (int i=0; i < length;i++) printf("................");
+    printf("\n");
+
+}
++ ++
+void print_board(char board[3][3])
+{
+    int cell = 0;
+
+    print_sep(3);
+    for (int row = 0; row < 3; ++row) {
+        for (int column = 0; column < 3; ++column) {
+            printf("\t | %d: %c ", cell, board[row][column]);
+            ++cell;
+        }
+        printf("\t | \n");
+        print_sep(3);
+    }
+}
+
+char get_winner(char board[3][3])
+{
+    char winner = '-';
+    
+    
+    return winner;
+}
+
+bool has_free_cell(char board[3][3])
+{
+    bool free_cell=false;
+    
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++) // Voy a necesitar cambier el j < 3 por un j < n.
+        {
+            if (board[i][j] == '-')
+            {
+                free_cell = true;
+            }
+            
+        }
+    }
+    
+    return free_cell;
+}
+
+int main(void)
+{
+    printf(VERDE_T "\t Ta Te Ti\n");
+
+    char board[3][3] = {
+        { '-', '-', '-' },
+        { '-', '-', '-' },
+        { '-', '-', '-' }
+    };
+
+    char turn = 'X';
+    char winner = '-';
+    int cell = 0;
+    while (winner == '-' && has_free_cell(board)) {
+        print_board(board);
+        printf("\nTurno %c - Elija posición (numero del 0 al %d): ", turn,
+               CELL_MAX);
+        int scanf_result = scanf("%d", &cell);
+        if (scanf_result <= 0) {
+            printf("Error al leer un numero desde teclado\n");
+            exit(EXIT_FAILURE);
+        }
+        if (cell >= 0 && cell <= CELL_MAX) {
+            int row = cell / 3;
+            int colum = cell % 3;
+            if (board[row][colum] == '-') {
+                board[row][colum] = turn;
+                turn = turn == 'X' ? 'O' : 'X';
+                winner = get_winner(board); 
+                /* Existe un error al momento de elegir al ganador*/
+            } else {
+                printf("\nCelda ocupada!\n");
+            }
+        } else {
+            printf("\nCelda invalida!\n");
+        }
+    }
+    print_board(board);
+    if (winner == '-') {
+        printf("Empate!\n");
+    } else {
+        printf("\n \t El ganador es : %c \n", winner);
+    }
+        printf(RESET_COLOR);
+        
+    return 0;
+}
