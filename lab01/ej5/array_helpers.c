c55d55430c33c513fc3c234d18474c81c0025d04
Rodrigo Flores <rodrigo.rf.flores@gmail.com>
2023-03-30 15:33:50 -0300

    lab01 concluido

---
+++ lab01/ej5/array_helpers.c
@@ -0,0 +1,83 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "mybool.h"
+#include "array_helpers.h"
+
+unsigned int array_from_file(
+    int array[],
+    unsigned int max_size,
+    const char *filepath
+    ){
+        
+    unsigned int length;
+    FILE * f;
+    f=fopen(filepath , "r");
+    if (f == NULL)
+    {
+        printf("Error al leer el fichero \n");
+        exit(EXIT_FAILURE);
+    }
+    
+    fscanf(f , "%u" , &length);
+
+    if (length > max_size)
+    {
+        exit(EXIT_FAILURE);
+    } 
+        
+    for (unsigned int i = 0; i < length; i++)
+    {
+       fscanf(f , "%d", &array[i]);
+    }
+    
+    fclose(f);
+
+    return length;
+}
+    
+void array_dump(
+    int a[], 
+    unsigned int length
+    ){
+
+    printf("[");
+    for (unsigned int i = 0; i < length; i++)
+    {
+        if (i < length - 1)
+        {
+            printf(" %d,", a[i]); 
+        } else {
+            printf(" %d", a[i]); 
+        }
+    }
+    printf("] \n");
+}
+
+mybool array_is_sorted(
+    int a[],
+    unsigned int length
+    ){
+
+    mybool ord = true;
+
+    for (unsigned int i = 0; i < (length - 1); i++)
+    {
+        if (a[i] <= a[i+1] && (ord == true))
+        {
+            ord = true;
+        } else {
+            ord = false;
+        }
+        
+    }
+
+    if (ord == true)
+    {
+        printf("El arreglo esta ordenado \n");
+    } else {
+        printf("El arreglo no esta ordenado \n");
+    }  
+
+    return ord;
+}
--- lab01/ej5/mybool.h
+++ lab01/ej5/mybool.h
@@ -1,5 +1,9 @@
 #define true 1
 #define false 0
 
+#ifndef MYBOOL
+#define MYBOOL
+
 typedef int mybool;
 
+#endif /*MYBOOL*/
