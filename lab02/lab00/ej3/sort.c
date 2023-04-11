3907b24784c1d7e687864dba974c223603011d89
Rodrigo Flores <rodrigo.rf.flores@gmail.com>
2023-04-11 15:13:01 -0300

    lab02 ej3

--- lab02/lab00/ej3/sort.c
+++ lab02/lab00/ej3/sort.c
@@ -9,18 +9,50 @@
 
 
 static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
-    /* Needs implementation */
 
-    /* Permutes elements of a[izq..der] and returns pivot such that:
-     - izq <= pivot <= der
-     - elements in a[izq,pivot) all 'go_before' (according to function goes_before) a[pivot]
-     - a[pivot] 'goes_before' all the elements in a(pivot,der]
-    */
+   unsigned int i = izq + 1;
+   unsigned int j = der;
+   unsigned int ppiv = izq;
+   
+   while (i <= j)
+   {
+        if (goes_before(a[i],a[ppiv]))
+        {
+            i++; 
+        } else if (goes_before(a[ppiv],a[j]))
+        {
+            j--; 
+        } else if ( !(goes_before(a[i],a[ppiv])) && !(goes_before(a[ppiv],a[j])))
+        {
+            swap(a,i,j);
+            i++;
+            j--;
+        }
+   }
+
+   swap(a,ppiv,j);
+   ppiv = j;
+
+   return ppiv;  
 }
 
 static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 3 */
+    
+    unsigned int ppiv;
+    ppiv = partition(a,izq,der);
 
+    if (der > izq)
+    {
+        if (ppiv > izq)
+        {
+            quick_sort_rec(a,izq,ppiv-1);
+        }
+
+        if (ppiv < der)
+        {
+            quick_sort_rec(a, ppiv+1,der);
+        }
+    }
 }
 
 void quick_sort(int a[], unsigned int length) {
