072aa3491ba77e24923377aebdf3f40b562b8495
Rodrigo F <roodrii.flores.rf@gmail.com>
2023-04-26 19:42:30 -0300

    lab03

--- lab03/ej2/proc2.c
+++ lab03/ej2/proc2.c
@@ -2,23 +2,21 @@
 #include <stdio.h>
 
 void absolute(int x, int *y) {
-    y = &x;
-    if(x<0){
-        x = -x;
+    *y = x;
+    if(x < 0) {
+        *y = -x;
     }
 }
 
 int main(void) {
-    int a=0, res=0;  // No modificar esta declaración1
+    int a = 0, res = 0;
     int *y = NULL;
     a = -10;
 
+    y = &res;
     absolute(a, y);
-    res = *y;
-    
-    printf("absolute = %d \n", res);
 
-    /* Violacion de segmento */
+    printf("absolute = %d \n", res);
 
     return EXIT_SUCCESS;
 }
