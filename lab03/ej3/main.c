6cbc712eeffe25c8ed4a0e02bed9f619b2f9da39
Rodrigo F <roodrii.flores.rf@gmail.com>
2023-04-30 16:57:07 -0300

    lab03 ej3

--- lab03/ej3/main.c
+++ lab03/ej3/main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #define MAX_SIZE 1000
 
@@ -12,30 +13,90 @@ static void dump(char a[], unsigned int length) {
     printf("\n\n");
 }
 
+char *parse_filepath(int argc, char *argv[]) {
+    /* Parse the filepath given by command line argument. */
+    char *result = NULL;
+    // Program takes exactly two arguments
+    // (the program's name itself and the input-filepath)
+    bool valid_args_count = (argc == 2);
+
+    if (!valid_args_count) {
+        printf("Error, debe de ingresar un archivo.in \n");
+        exit(EXIT_FAILURE);
+    }
+
+    result = argv[1];
+
+    return result;
+}
+
 unsigned int data_from_file(
     const char *path,
     unsigned int indexes[],
-    schar letters[],
+    char letters[],
     unsigned int max_size
     ){
     
+    FILE *file;
+    file = fopen(path, "r");
 
-
+    if (file == NULL)
+    {
+        printf("Error al leer el archivo \n");
+        exit(EXIT_FAILURE);
+    }
     
+    unsigned int i = 0;
+    
+    while ((!feof(file)) && i < max_size)
+    {
+        int ctrl = fscanf(file,"%u -> *%c*\n", &indexes[i], &letters[i]);
+
+        if (ctrl != 2)
+        {
+            printf("Error de argumentos \n");
+            exit(EXIT_FAILURE);
+        }
+        
+        if (indexes[i] > max_size)
+        {
+            printf("Error, supero el limite del arreglo. \n");
+            exit(EXIT_FAILURE);
+        }
+        
+        i++;
+    }
+
+    return i;
 }
 
+void sort(
+    unsigned int length,
+    unsigned int indexes[],
+    char letters[],
+    char sorted[]
+    ){
+    
 
-int main(/* -- completar-- */) {
-    FILE *file;
+    for (unsigned int i = 0; i < length; i++)
+    {
+        int n = indexes[i];
+        sorted[n] = letters[i];
+    }
+}
+
+int main(int argc, char *argv[]) {
+    char *file = NULL;
     unsigned int indexes[MAX_SIZE];
     char letters[MAX_SIZE];
     char sorted[MAX_SIZE];
-    unsigned int length=0; 
-    //  .----------^
-    //  :
-    // Debe guardarse aqui la cantidad de elementos leidos del archivo
+    unsigned int length=0;
+
+    file = parse_filepath(argc, argv);
     
-    /* -- completar -- */
+    length = data_from_file(file,indexes,letters,MAX_SIZE);
+
+    sort(length,indexes,letters,sorted);
 
     dump(sorted, length);
 
